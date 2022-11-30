7516ad993ce2cd5731a1c435ead18bac9dbfe005
muebleriamacj1 <119469856+muebleriamacj1@users.noreply.github.com>
2022-11-29 21:49:02 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+     apiKey: "AIzaSyCaBlp1LUNjqMu9oGPokC-W3Bf7yoY8n60",
+  authDomain: "muebleriamacj1-87e03.firebaseapp.com",
+  projectId: "muebleriamacj1-87e03",
+  storageBucket: "muebleriamacj1-87e03.appspot.com",
+  messagingSenderId: "782628201205",
+  appId: "1:782628201205:web:c884ad5a67a53359705bbc",
+  measurementId: "G-1GEYQHHLDZ"
+});
